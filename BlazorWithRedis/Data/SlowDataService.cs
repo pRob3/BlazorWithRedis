4689a498750c1f4d65d55eb31544129682c0daee
pRob3 <bubblan@slaf.se>
2022-09-11 11:19:50 +0200

    fetch data

---
+++ BlazorWithRedis/Data/SlowDataService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorWithRedis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWithRedis.Data
+{
+    public class SlowDataService
+    {
+        private readonly DatabaseContext _context;
+
+        public SlowDataService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public List<User> GetUsers()
+        {
+            return _context.Users
+                .Where(x => x.FirstName.Contains("john") && x.LastName.Contains("sm")).Take(1000)
+                .ToList();
+        }
+    }
+}
---
+++ BlazorWithRedis/Pages/FetchNamesSlow.razor.cs
@@ -0,0 +1,31 @@
+﻿using BlazorWithRedis.Data;
+using BlazorWithRedis.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWithRedis.Pages
+{
+    partial class FetchNamesSlow
+    {
+        [Inject] DatabaseContext Context { get; set; }
+
+        private List<User> slowData;
+        private string loadLoacation = string.Empty;
+        private string isCacheData = string.Empty;
+
+        private async Task LoadData()
+        {
+            slowData = null;
+            SlowDataService slowDataService = new SlowDataService(Context);
+            slowData = await Task.Run(() => slowDataService.GetUsers());
+
+            loadLoacation = $"Loaded from db at {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff")}";
+            isCacheData = "";
+        }
+    }
+}
