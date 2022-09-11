e2ff72d11fae039f8777125a83508fdd17ca990f
pRob3 <bubblan@slaf.se>
2022-09-11 10:30:02 +0200

    Add project files.

---
+++ BlazorWithRedis/Migrations/20220911075713_insertMillionNamesToDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWithRedis.Migrations
+{
+    public partial class insertMillionNamesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                migrationBuilder.Sql($"INSERT INTO Users (FirstName, LastName) VALUES ('John {i}', 'Smith {i}');");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Users;");
+        }
+    }
+}
---
+++ BlazorWithRedis/Models/DatabaseContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWithRedis.Models
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<User> Users { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+
+                entity.Property(e => e.Id);
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
